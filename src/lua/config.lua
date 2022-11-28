85a47142374e3fafe4ff41d4e2e69a00a0b39d5f
ci-appveyor <CI@appveyor.com>
2022-11-28 18:47:03 +0100

    config.lua

--- src/lua/config.lua
+++ src/lua/config.lua
@@ -1,15 +1,11 @@
 function SYN_process(x,i,samples)
-    note=-24+ ((rand %3)*12)
-    y = fm_feedback.process( i, hz(note), 1.0, hz(note), math.sin(i/1500), hz(note+12), math.cos(i/rand/1500) )
-    return y
+    note=-24 --+ ((rand %3)*12)
+    y = clamp( fm_feedback.process( i, hz(note), 1.0, hz(note), math.sin(i/1700)*0.7, hz(note+12), math.cos(i/rand/1700) ) * 2)
+    return excite.process(y)
 end
 
 -- billion dollar feedback FM synth
 fm_feedback = {
-    feedback = 1.3,    -- min=0, max=4, step=0.001
-    decay    = 0.05,   -- min=0.0001, max=0.1 ,step=0.0001
-    phase    = 0,      -- 
-    attack   = 0.0001, -- 
     v        = 0,      -- last value 
     process = function(i,f1,a1,f2,a2,f3,a3)
         fm_feedback.v = math.sin( ((2*math.pi)*(i*f1)/srate) ) * a1
@@ -26,13 +22,58 @@ function hz(note)
 end
 
 function fwrap(x)
-    if (x > 1)  then return math.fmod(x,1) - 1 end 
-    if (x < -1) then return 1 - math.fmod(-x,1) end 
-    return x
+  b = x
+  if( x < 0 ) then b = -x  end
+  z,c = math.modf(b,1)
+  if (x > 1)  then return clamp(x) - c end
+  if (x < -1) then return clamp(x) + c end
+  return x
 end
 
 function fwrap2(x)
     return fwrap( fwrap(x) )
 end
 
-print('config.lua inited')
+lpf = {
+  s0=0,
+  s1=0,
+  a0=0,
+  q=0.3, -- 0:1
+  process = function(x,cutoff,q)
+    if not q == nil then lpf.q = q end
+    cutoff = 2.0*math.sin(math.pi*cutoff/srate)
+    fb = lpf.q + lpf.q/(1.0 - cutoff )
+    hp = x - lpf.s0
+    bp = lpf.s0 - lpf.s1
+    lpf.s0 = lpf.s0 + cutoff * (hp + fb * bp)
+    lpf.s1 = lpf.s1 + cutoff * (lpf.s0 - lpf.s1)
+    return lpf.s1
+  end
+}
+
+function clamp(y)
+  if( y >  1 ) then y =  1.0 end
+  if( y < -1 ) then y = -1.0 end
+  return y
+end
+
+hpf = {
+  process = function(x,cutoff,q)
+    return lpf.process(x,cutoff,q) - x
+  end
+}
+
+excite = {
+  s0=0,
+  process= function(x)
+    y = fwrap(x + (x - excite.s0*1.1))
+    excite.s0 = y
+    return y
+  end
+}
+
+
+
+print('config.lua initedd')
+print( fwrap(1.2) )
+print( fwrap(-1.2) )
